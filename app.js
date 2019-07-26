44027f1898072828a7d142f11c4110b7914c5f86
ismail <ismailsoylemez272@gmail.com>
2019-07-26 19:12:56 +0300

    emit ile data göndermek

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const socketio = require('socket.io');
+
 const server = http.createServer((req,res) => {
     res.end('hey');
 });
@@ -12,13 +13,14 @@ io.sockets.on('connection' , (socket) => {
     console.log('kullanıcı bağlandı');
 
     //sunucudan cliente emit yollarken bu şekilde
-    setTimeout(() => {
-        socket.emit(('merhabaDe'));
-    },2000);
+    /*setInterval(() => {
+        socket.emit('merhabaDe', { country: 'Turkey'} );
+    },1000);*/
 
     //clienttan gelen emiti sunucuda socket.on ile karşılıyoruz
-    socket.on('selamVer', () => {
-        console.log('selam');
+    socket.on('selamVer', (data) => {
+        console.log(data.city);
+        console.log(data.name);
     });
 
     socket.on('disconnect', () => {
